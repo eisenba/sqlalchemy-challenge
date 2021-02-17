30c21b4a528d41f3972d596104742a543338c17c
Matt Eisenbart <matt.eisenbart@gmail.com>
2021-02-17 01:51:09 -0600

    Add /precipitation to Flask

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, desc
+import datetime as dt
 # Import Flask
 # from flask import Flask, jsonify
 from flask import Flask, render_template, redirect, url_for, jsonify
@@ -30,14 +31,45 @@ def welcome():
     """List all available api routes."""
     return (
         f"Welcome to the Hawaii Weather Api<br/>"
-        f"Available Routes: :<br/>"
+        f"Available Routes: <br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/&lt;start&gt;<br/>"
-        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br/>"
+        f"/api/v1.0/&lt; start &gt;<br/>"
+        f"/api/v1.0/&lt; start &gt;/&lt; end &gt;<br/>"
     )
 
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+# Create our session (link) from Python to the DB
+    session = Session(engine)
+    # Find lastest date
+    lastest_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
+    lastest_year = int(lastest_date[0][:4])
+    lastest_month = int(lastest_date[0][5:7])
+    lastest_day = int(lastest_date[0][-2:])
+    lastest_date
+    # Find oldest date
+    oldest_date = dt.date(lastest_year, lastest_month, lastest_day) - dt.timedelta(days=365)
+    oldest_date
+    # Query database
+    precipitation = session.query(measurement.date, measurement.prcp).\
+    filter(measurement.date > oldest_date).\
+    order_by(measurement.date).all()
+
+    session.close()
+    # Build response
+    preciparraymap = []
+
+    for date,prcp in precipitation:
+        precipdictmap = {}
+        precipdictmap["date"] = date
+        precipdictmap["precip"] = prcp
+        preciparraymap.append(precipdictmap)
+    return jsonify(preciparraymap)
+
+@app.route("/api/v1.0/stations")
+def station():
 
 if __name__ == "__main__":
     app.run(debug=True)
